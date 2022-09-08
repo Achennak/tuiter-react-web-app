d693b12d85d0acfd23e7f1fa896e59d41d851bcc
Akshaya <achennak@Akshayas-MacBook-Air.local>
2022-09-07 21:06:23 -0400

    2022 Web Dev First Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './icons8-deathly-hallows-100.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          I Solemnly Swear That I Am Up to No Good
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
