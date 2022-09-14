e2980d1975f3049253415bb259ffcf4f78f2e7d1
Akshaya <achennak@Akshayas-MacBook-Air.local>
2022-09-14 12:27:23 -0400

    Created a2 folder and added changes for assignement2

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './icons8-deathly-hallows-100.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+         I solemnly swear that i am up to no good !!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
