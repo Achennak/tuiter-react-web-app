5e8660dcce3d8904c0822c10d93c94084ed167d6
Akshaya <achennak@Akshayas-MacBook-Air.local>
2022-10-16 12:24:16 -0400

    Minor changes

--- public/tuiter/HomeScreen/PostList/PostItem.js
+++ public/tuiter/HomeScreen/PostList/PostItem.js
@@ -7,17 +7,21 @@ export default   [
         profileImg: '../../images/moon.jpeg',
         postImg: "../../images/falcon.jpeg",
         postImgContentTitle: "SpaceX releases the FALCON 2023.JavaScript is programming language that can run on browsers as well as desktops.",
-        postImgContent: "SpaceX release enjoy"
+        postImgContent: "SpaceX release enjoy.HTML and CSS on their own can create compelling static Web pages.We'll write JavaScript code that can react to user events, can generate dynamic HTML, and modify the DOM programmatically.",
+        postLink:"netflix.com",
+        postExternalLink:"@InspirationMission"
 
     },
     {
         profileName: 'New York Post',
         handleName: '@nyPost',
         date: 'Yesterday',
-        content: 'House of the Dragon blows up social media with fans in tears https://trib.al/HqEiwZo',
+        content: 'House of the Dragon blows up social media with fans in tears ',
         profileImg: '../../images/fall.jpeg',
         postImg: "../../images/hd.jpeg",
         postImgContentTitle: "",
-        postImgContent: ""
+        postImgContent: "",
+        postLink:"",
+        postExternalLink:"https://trib.al/HqEiwZo",
     },
 ];
--- public/tuiter/HomeScreen/PostList/PostList.js
+++ public/tuiter/HomeScreen/PostList/PostList.js
@@ -22,6 +22,10 @@ const PostList = (tweet) => {
             <div class="wd-profile-details">
                 <div class="wd-bio">
                     <p class="wd-text-bio"> ${tweet.content}</p>
+                    ${tweet.postExternalLink ? `<span class="wd-text-bio text-primary"><a>  ${tweet.postExternalLink}</a></span>` : ``}
+                   
+                   
+                    
                 </div>
             </div>
             <div class="wd-bookmark-info" id="image-info">
@@ -35,13 +39,13 @@ const PostList = (tweet) => {
                     <!--horizontal text below image-->
                     ${tweet.postImgContentTitle
                       ? `<p class="wd-title">  ${tweet.postImgContentTitle}</p>` : ``}
-                    <p className="wd-text-below-image">  ${tweet.postImgContent}</p>
-                   <!-- ${tweet.postImgContent
-                      ? `<p className="wd-text-below-image">  ${tweet.postImgContent}</p>` : ``}-->
-                   
-                    <p class="wd-link-1">
-                        <i class="fa fa-solid fa-link"></i> netflix.com
-                    </p>
+                    
+                    ${tweet.postImgContent
+                      ? `<p class="wd-text-below-image">  ${tweet.postImgContent}</p>` : ``}
+                    
+                   ${tweet.postLink ? `<p class ="wd-link-1"><i class="fa fa-solid fa-link"></i>${tweet.postLink}</p>` : ``}
+            
+                    
                 </div>
             </div>
             <div class="wd-emojis">
@@ -88,78 +92,5 @@ const PostList = (tweet) => {
         </div>
     </div>`);
 };
-
-
-
-
-
-
-
-
-
-
- /*   return(`
-        <div class="wd-post_container">
-            <div class="wd-B__image">
-                <img class="wd-B__profile-img" src=${tweet.profileImg}>
-            </div>
-            <div class="wd-mid-segment-container">
-                <div class="wd-C_container">
-                    <div class="wd-C__profile-info">
-                        <h4 class="wd-C__profile-name">${tweet.profileName} <i class="fas fa-check-circle" aria-hidden="true"></i></h3> 
-                        <h4 class="wd-C__handle-name">${tweet.handleName}</h4>
-                        <h4 class="wd-C__handle-name">&nbsp;<span class="wd-dot">&#183;</span>&nbsp;${tweet.date}</h4>
-                    </div>
-                    <div class="wd-C__options">
-                        ...
-                    </div>
-                </div>
-                <div class="wd-D__container">
-                    <div class="wd-D__text">
-                        <p>
-                            ${tweet.content}
-                        </p>  
-                    </div>
-                </div>
-                <div class="wd-E-F__container" id="image-info">
-                    <div class="wd-E__image">
-                        <img src=${tweet.postImg} width="100%"/>
-                    </div>
-                    <div class="wd-F__container">
-                            <div class="wd-F__title">
-                                ${tweet.postImgContentTitle}
-                            </div>
-                            <div class="wd-F__text">
-                                ${tweet.postImgContent}
-                            </div>
-                    </div>
-                </div>
-                <div class="wd-G_container">
-                    <div class="wd-post-info">
-                            <p class="wd-icons">
-                                <a href="#" class="wd-link"><i class="fa-regular fa-comment"></i><p class="wd-numbers">123</p></a>
-                            </p>
-                    </div>
-                    <div class="wd-post-info">
-                        <p class="wd-icons">
-                            <a href="#" class="wd-link"><i class="fa-solid fa-retweet"></i><p class="wd-numbers">123</p></a>
-                        </p>
-                    </div>
-                    <div class="wd-post-info">
-                        <p class="wd-icons">
-                            <a href="#" class="wd-link"> <i class="fa-solid fa-heart red"></i><p class="wd-numbers">123</p></a>
-                        </p>    
-                    </div>
-                    <div class="wd-post-info">
-                        <p class="wd-icons">
-                            <a href="#" class="wd-link"><i class="fa-solid fa-arrow-up-from-bracket"></i></a>
-                        </p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    `);
-
-};*/
 
 export default PostList;
