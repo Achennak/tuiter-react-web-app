54a2e2fa344a732b9b521e7ddf9d257797c40636
Akshaya <achennak@Akshayas-MacBook-Air.local>
2022-10-22 21:48:43 -0400

    minor changes

--- src/tuiter/Home/HomeComponent/tweetList.js
+++ src/tuiter/Home/HomeComponent/tweetList.js
@@ -3,15 +3,14 @@ import React from "react";
 import tweets from './tweets.json';
 import TweetItem from "./tweetItem";
 
-
 const TweetList = () => {
-    return(
+    return (
         <ul className="list-group">
             {
                 tweets.map(tweet => <TweetItem key={tweet._id} tweet={tweet}/>)
             }
         </ul>
-);
+    );
 };
 
 export default TweetList;
