b0ebdc565763838d47a77faeb90f019703b264c9
Akshaya <achennak@Akshayas-MacBook-Air.local>
2022-10-23 20:25:36 -0400

    Minor Changes

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -4,23 +4,26 @@ import ExploreComponent from "./ExploreComponent/index";
 import React from "react";
 import {Routes, Route} from "react-router";
 import HomeComponent from "./home/index";
+
 function Tuiter() {
-    return(
+    return (
 
         <div className="row mt-2">
             <div className="col-xl-2 col-xxl-2 col-sm-2 col-md-2 col-lg-1">
-            <NavigationSidebar active="explore"/>
+                <NavigationSidebar active="explore"/>
             </div>
-            <div className="col-xxl-6 col-xl-6 col-lg-7 col-md-10 col-sm-10" style={{"position": "relative"}}>
+            <div className="col-xxl-6 col-xl-6 col-lg-7 col-md-10 col-sm-10"
+                 style={{"position": "relative"}}>
                 <Routes>
-                    <Route path="home"    element={<HomeComponent/>}/>
+                    <Route path="home" element={<HomeComponent/>}/>
                     <Route path="explore" element={<ExploreComponent/>}/>
                 </Routes>
             </div>
             <div className="d-none d-lg-block d-xl-block d-xxl-block col-4">
-            <WhoToFollowList/>
+                <WhoToFollowList/>
             </div>
         </div>
     );
 }
+
 export default Tuiter;
